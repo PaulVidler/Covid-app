b5540297efb95272803678278f98ff9c23b034c2
Paul Vidler <45616638+PaulVidler@users.noreply.github.com>
2020-07-05 09:20:52 +1000

    Update card.jsx

--- covid-app/src/Components/Cards/Cards.jsx
+++ covid-app/src/Components/Cards/Cards.jsx
@@ -1,22 +1,58 @@
 import React from 'react'
 import {Card, CardContent, Typography, Grid, StylesProvider} from '@material-ui/core'
 import styles from './Cards.module.css'
+import CountUp from 'react-countup';
 
 
-const cards = (props) => {
-    console.log(props);
-
-    
+const cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
+    //console.log(props);
+    if(!confirmed) {
+        return 'Loading...';
+    }
 
     return (
-        <div className={StylesProvider.container}>
+        <div className={styles.container}>
             <Grid container spacing={3} justify="center">
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>     
-                        <Typography variant="h5" color="textSecondary">REAL DATA</Typography>     
-                        <Typography color="textSecondary">REAL DATE</Typography>     
+                        <Typography variant="h5">
+                            <CountUp
+                                start={10049505}
+                                end={confirmed.value}
+                                duration={2.5}
+                                separator="," />
+                        </Typography>
+                        <Typography color="textSecondary">{lastUpdate}</Typography>     
                         <Typography variant="body2">Number of active cases</Typography>     
+                    </CardContent>    
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>     
+                        <Typography variant="h5" color="textSecondary">
+                        <CountUp
+                                start={5000000}
+                                end={recovered.value}
+                                duration={2.5}
+                                separator="," />
+                                </Typography>     
+                        <Typography color="textSecondary">REAL DATE</Typography>     
+                        <Typography variant="body2">Number of recovered cases</Typography>     
+                    </CardContent>    
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>     
+                        <Typography variant="h5" color="textSecondary">
+                        <CountUp
+                                start={100000}
+                                end={deaths.value}
+                                duration={2.5}
+                                separator="," />
+                            </Typography>     
+                        <Typography color="textSecondary">REAL DATE</Typography>     
+                        <Typography variant="body2">Number of deaths</Typography>     
                     </CardContent>    
                 </Grid>
             </Grid>
